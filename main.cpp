5b74fc28d7db7ed32bfa0ca17e69ecb5903d4682
Phillip Wagner <m0ssyblue7@gmail.com>
2025-10-26 13:14:45 -0700

    Seventh addition to Lab-23

--- main.cpp
+++ main.cpp
@@ -17,27 +17,64 @@ int main_menu();
 
 int main() {
     srand(time(0));
-    bool again;
 
     string names[SZ_NAMES], colors[SZ_COLORS];
 
     ifstream fin("names.txt");
     if (!fin) {
-        cerr << "Error: names.txt not found!";
+        cerr << "Error: names.txt not found";
         return 1;
     }
 
     int i = 0;
+    //changes made to not have oiverflow and to have exact counting
     while (i < SZ_NAMES && fin >> names[i]) i++;
+    int num_names = i;
     fin.close();
+
     ifstream fin1("colors.txt");
-    string colors[SZ_COLORS];
+    if (!fin2) {
+        cerr << "Error: colors.txt not found";
+        return 1;
+    }
+//same changes as above
     i = 0;
-    while (fin1 >> colors[i++]);
+    while (i < SZ_COLORS && fin2 >> colors[i]) i++;
+    int num_colors = i;
     fin1.close();
 
+list <Goat> trip;
 
+bool running = true;
+while (running) {
+    int choice = main_menu();
 
-
+    switch (choice) {
+        case 1;
+            add_goat(trip, names, colors);
+            break;
+        case 2:
+            delete_goat(trip);
+            break;
+        case 3:
+            display_trip(trip);
+            break;
+        case 4:
+            running = false;
+            cout << "goodbye";
+            break;
+    }
+}
     return 0;
+}
+
+int main_menu() {
+    int choice;
+    cout << "";
+    cout << "";
+    cout << "";
+    cout << "";
+    cout << "";
+    cout << "";
+    cout << "";
 }
