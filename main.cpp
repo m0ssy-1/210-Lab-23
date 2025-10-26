f3905ddd9bb31fd72932c24b9a1c234c9cf2149d
Phillip Wagner <m0ssyblue7@gmail.com>
2025-10-26 12:53:30 -0700

    Fourth addition to lab-23

--- main.cpp
+++ main.cpp
@@ -3,23 +3,31 @@
 #include <iomanip>
 #include <list>
 #include "Goat.h"
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
 
-int select_goat(list<Goat> trip);
+int select_goat(const list<Goat> trip);
 void delete_goat(list<Goat> &trip);
 void add_goat(list<Goat> &trip, string [], string []);
-void display_trip(list<Goat> trip);
+void display_trip(const list<Goat> trip);
 int main_menu();
 
 int main() {
     srand(time(0));
     bool again;
 
+    string names[SZ_NAMES], colors[SZ_COLORS];
+
     // read & populate arrays for names and colors
     ifstream fin("names.txt");
-    string names[SZ_NAMES];
+    if (!fin) {
+        cerr << "Error: names.txt not found!";
+        return 1;
+    }
+
     int i = 0;
     while (fin >> names[i++]);
     fin.close();
